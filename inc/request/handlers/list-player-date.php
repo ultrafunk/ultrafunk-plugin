ea8363c7351c930c9ff9621e0cd2b2467f784637
ultrafunk.com <rune@ultrafunk.com>
2022-10-16 02:42:13 +0200

    1.44.55

--- inc/request/handlers/list-player-date.php
+++ inc/request/handlers/list-player-date.php
@@ -26,7 +26,7 @@ class ListPlayerDate extends \Ultrafunk\Plugin\Request\RequestHandler
     if (checkdate($month, 1, $year))
     {
       $this->request_params['get']['date'] = true;
-      $this->route_path   = "list/$year/$month";
+      $this->route_path   = "list/$year/{$this->route_request->path_parts[2]}";
       $this->title_parts  = ['prefix' => 'Channel', 'title' => date('F Y', mktime(0, 0, 0, $month, 1, $year))];
       $this->current_page = $this->get_current_page($this->route_request->path_parts, 4);
       $this->query_args   = ['date_query' => ['year' => $year, 'month' => $month]];
